9bb21257cdc2e08a2409495e86aec2d91638385e
jeffward01 <jeffwardcoding@gmail.com>
2016-01-10 17:49:15 -0800

    working add url page

---
+++ LinkHub/LinkHubUI/LinkHubUI/Controllers/URLController.cs
@@ -0,0 +1,20 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+    }
+}
